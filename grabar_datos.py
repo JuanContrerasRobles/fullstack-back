0e0237259c95b2dbf7a8e2d5aa7a524a7d5dfcb1
Juan <79379448+JuanContrerasRobles@users.noreply.github.com>
2022-05-20 22:41:56 +0200

    Añado el archivo grabar_datos.py al repositorio back

---
+++ grabar_datos.py
@@ -0,0 +1,4 @@
+def grabar_datos(nombre, apellidos):
+        with open("pedidos.txt", "a", encoding="utf-8") as file:
+            file.write(nombre+ " " + apellidos + "\n")
+            file.close()
