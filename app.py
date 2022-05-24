82c4cecbc479e27bad8bb174380af3173e604db4
Juan <79379448+JuanContrerasRobles@users.noreply.github.com>
2022-05-24 18:23:02 +0200

    Se actualiza app.py para Pylint

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+""" Se importan funciones y datos desde la libreria"""
 from flask import Flask, request, redirect
 from grabar_datos import grabar_datos
 
@@ -6,17 +7,11 @@ app = Flask(__name__)
 @app.route("/pizza", methods=['POST'])
 
 def datos():
-
+    """Funcion que recoge los datos a traves del metodo POST"""
     if request.method == "POST":
-
         nombre = request.form.get("Nombre")
-        
         apellidos = request.form.get("Apellidos")
-        
         usuario = nombre + " " + apellidos
-        
         grabar_datos(nombre, apellidos)
-
         print(usuario)
-        
     return redirect ("http://localhost/solicita_pedido.html", code=302)
