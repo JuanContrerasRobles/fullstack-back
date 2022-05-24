38305de5efbfac5e1c9beb6982abb74110560c3f
Juan <79379448+JuanContrerasRobles@users.noreply.github.com>
2022-05-24 18:37:47 +0200

    Se actualiza grabar_datos.py para Pylint

--- grabar_datos.py
+++ grabar_datos.py
@@ -1,4 +1,5 @@
+"""Funcion para grabar datos de pedido"""
 def grabar_datos(nombre, apellidos):
-        with open("pedidos.txt", "a", encoding="utf-8") as file:
-            file.write(nombre+ " " + apellidos + "\n")
-            file.close()
+    with open("pedidos.txt", "a", encoding="utf-8") as file:
+        file.write(nombre+ " " + apellidos + "\n")
+        file.close()
