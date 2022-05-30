cc2c2cb519b7c6173147daecb3286a8edc6a7fa7
Juan <79379448+JuanContrerasRobles@users.noreply.github.com>
2022-05-30 20:54:13 +0200

    Código prueba app.py para chequear tamaño "S"
    Haga lo que haga siempre me retorna "disponible" y me he quedado estancado, necesito un empujoncito.

--- app.py
+++ app.py
@@ -15,3 +15,29 @@ def datos():
         grabar_datos(nombre, apellidos)
         print(usuario)
     return redirect ("http://localhost/solicita_pedido.html", code=302)
+
+def checksize():
+    """Comprueba disponibilidad de un tamaño de pizza"""
+    if request.method == "POST":
+        mensaje1 = "Disponible"
+        mensaje2 = "No Disponible"
+        mensajes = mensaje1, mensaje2
+        if request.form.get("S") == False:
+            print(mensaje1)
+        else:
+            print(mensaje2)
+    return Response(mensajes, 200,{'Access-Control-Allow-Origin': '*'})
+
+#Este es otro tipo de codigo pero siempre me retorna disponible ⇩
+
+"""     
+def checksize():
+    #Comprueba disponibilidad de un tamaño de pizza
+    if request.method == "POST": 
+    mensajes = {"Estado1":"Disponible","Estado2":"No disponible"}
+    for valor in mensajes.values():
+        if request.form.get != "S":
+            print(valor)
+            return valor
+    return Response(valor, 200,{'Access-Control-Allow-Origin': '*'})
+"""
